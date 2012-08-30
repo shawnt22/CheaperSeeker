e0e1b440de7853c656847d67224872e6334342c6
tengsong <shawnt22@gmail.com>
2012-08-30 18:54:25 +0800

    添加 ListDataStore 处理逻辑

--- CheaperSeeker/CheaperSeeker/Classes/Network/SListDataStore.h
+++ CheaperSeeker/CheaperSeeker/Classes/Network/SListDataStore.h
@@ -7,7 +7,75 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "SDataLoader.h"
 
-@interface SListDataStore : NSObject
+#pragma mark - ListDataStoreProtocol
+typedef struct {
+    NSInteger originalIndex;
+    NSInteger currentIndex;
+}PItemLocation;
+NS_INLINE PItemLocation PItemLocationMake(NSInteger oIndex, NSInteger cIndex) {
+    PItemLocation location;
+    location.originalIndex = oIndex;
+    location.currentIndex = cIndex;
+    return location;
+}
+#define InvalidItemLocaition PItemLocationMake(-1, -1)
+NS_INLINE BOOL isInvalidItemLocation(PItemLocation location) {
+    BOOL result = NO;
+    PItemLocation invalid = InvalidItemLocaition;
+    if (invalid.originalIndex == location.originalIndex && invalid.currentIndex == location.currentIndex) {
+        result = YES;
+    }
+    return result;
+}
+
+@protocol PListDataStoreProtocol <NSObject>
+@optional
+@property (nonatomic, retain) NSMutableArray *items;
+- (void)refreshItemsWithCachePolicy:(ASICachePolicy)cachePolicy URL:(NSString *)url;
+- (void)loadMoreItemsWithURL:(NSString *)url;
+- (BOOL)canLoadMore;
+//  PItemLocation 无效时，意味着未对原list进行任何处理
+- (PItemLocation)deleteItem:(id)item;
+- (PItemLocation)updateItem:(id)item;
+- (PItemLocation)insertItem:(id)item atIndex:(NSInteger)index needCheck:(BOOL)needCheck;
+@end
+
+#pragma mark - ListDataStore
+
+#define kListDataStoreRefreshCursor @"0"
+#define kListDataStoreRefreshPage   1
+#define kListDataStoreLimitCount 20
+
+#define kListDataStoreResponseItems     @"items"
+#define kListDataStoreResponseCount     @"count"
+#define kListDataStoreResponseCursor    @"cursor"
+
+@protocol PListRefreshLoadmoreProtocol <NSObject>
+@required
+- (void)refreshItemsWithCachePolicy:(ASICachePolicy)cachePolicy;
+- (void)loadmoreItems;
+@end
+
+@protocol PListDataStoreDelegate <NSObject>
+@required
+- (id)isExsitedItem:(id)item;
+@end
+
+typedef enum {
+    PListDataStoreStyleCursor,
+    PListDataStoreStylePage,
+}PListDataStoreStyle;
+
+@interface PListDataStore : SDataLoader <PListDataStoreProtocol> {
+@protected
+    BOOL _canLoadMore;
+}
+@property (nonatomic, assign) id<PListDataStoreDelegate> storeDelegate;
+@property (nonatomic, retain) NSString *cursorID;
+@property (nonatomic, assign) NSInteger pageIndex;
+@property (nonatomic, assign) NSInteger limitCount;
+@property (nonatomic, assign) PListDataStoreStyle storeStyle;
 
 @end
--- CheaperSeeker/CheaperSeeker/Classes/Network/SListDataStore.m
+++ CheaperSeeker/CheaperSeeker/Classes/Network/SListDataStore.m
@@ -8,6 +8,146 @@
 
 #import "SListDataStore.h"
 
-@implementation SListDataStore
+#pragma mark - ListDataStore
+@interface PListDataStore()
+@property (nonatomic, retain) SURLRequest *listRequest;
+- (id)notifyIsExsitedItem:(id)item;
+@end
+
+@implementation PListDataStore
+@synthesize storeDelegate;
+@synthesize items;
+@synthesize cursorID, pageIndex, limitCount;
+@synthesize listRequest;
+@synthesize storeStyle;
+
+#pragma mark init & dealloc
+- (void)initSubobjects {
+    [super initSubobjects];
+    self.storeDelegate = nil;
+    self.items = [NSMutableArray array];
+    _canLoadMore = YES;
+    self.storeStyle = PListDataStoreStyleCursor;
+    self.cursorID = kListDataStoreRefreshCursor;
+    self.pageIndex = kListDataStoreRefreshPage;
+    self.limitCount = kListDataStoreLimitCount;
+    self.listRequest = nil;
+}
+- (void)dealloc {
+    self.listRequest = nil;
+    self.storeDelegate = nil;
+    self.items = nil;
+    self.cursorID = nil;
+    [super dealloc];
+}
+
+#pragma mark request delegate
+- (void)cancelAllRequests {
+    [super cancelAllRequests];
+    [self cancelRequest:self.listRequest];
+}
+- (void)strequestFinished:(SURLRequest *)request {
+    switch (request.tag) {
+        case SURLRequestItemsRefresh:{
+            NSArray *_items = [request.responseData objectForKey:kListDataStoreResponseItems];
+            NSInteger _count = [[request.responseData objectForKey:kListDataStoreResponseCount] integerValue];
+            if ([_items count] > 0) {
+                [self notifyRequest:request submitResponse:_items];
+                self.items = [NSMutableArray arrayWithArray:_items];
+                if (self.storeStyle == PListDataStoreStyleCursor) {
+                    self.cursorID = [request.responseData objectForKey:kListDataStoreResponseCursor];
+                }
+            }
+            _canLoadMore = _count > [_items count] ? YES : NO;
+        }
+            break;
+        case SURLRequestItemsLoadmore:{
+            NSArray *_items = [request.responseData objectForKey:kListDataStoreResponseItems];
+            NSInteger _count = [[request.responseData objectForKey:kListDataStoreResponseCount] integerValue];
+            if ([_items count] > 0) {
+                [self notifyRequest:request submitResponse:_items];
+                [self.items addObjectsFromArray:_items];
+                if (self.storeStyle == PListDataStoreStyleCursor) {
+                    self.cursorID = [request.responseData objectForKey:kListDataStoreResponseCursor];
+                }
+            }
+            _canLoadMore = _count > [_items count] ? YES : NO;
+        }
+            break;
+        default:
+            break;
+    }
+    [super notifyRequestFinished:request];
+}
+
+#pragma mark items manager
+- (PItemLocation)deleteItem:(id)item {
+    PItemLocation location = InvalidItemLocaition;
+    id exsited = [self notifyIsExsitedItem:item];
+    if (exsited) {
+        NSInteger index = [self.items indexOfObject:exsited];
+        location.originalIndex = index;
+        [self.items removeObject:exsited];
+    }
+    return location;
+}
+- (PItemLocation)updateItem:(id)item {
+    PItemLocation location = InvalidItemLocaition;
+    id exsited = [self notifyIsExsitedItem:item];
+    if (exsited) {
+        NSInteger index = [self.items indexOfObject:exsited];
+        location.originalIndex = index;
+        location.currentIndex = index;
+        [self.items replaceObjectAtIndex:index withObject:item];
+    }
+    return location;
+}
+- (PItemLocation)insertItem:(id)item atIndex:(NSInteger)index needCheck:(BOOL)needCheck {
+    PItemLocation location = InvalidItemLocaition;
+    location.currentIndex = index;
+    if (needCheck) {
+        id existed = [self notifyIsExsitedItem:item];
+        if (existed) {
+            NSInteger existedIndex = [self.items indexOfObject:existed];
+            location.originalIndex = existedIndex;
+            index = index > existedIndex ? index - 1 : index;
+            [self.items removeObject:existed];
+        }
+    }
+    [self.items insertObject:item atIndex:index];
+    return location;
+}
+- (id)notifyIsExsitedItem:(id)item {
+    if (self.storeDelegate && [self.storeDelegate respondsToSelector:@selector(isExsitedItem:)]) {
+        return [self.storeDelegate isExsitedItem:item];
+    }
+    return nil;
+}
+- (BOOL)canLoadMore {
+    return _canLoadMore;
+}
+- (void)refreshItemsWithCachePolicy:(TTURLRequestCachePolicy)cachePolicy URL:(NSString *)url {
+    [self cancelRequest:self.listRequest];
+    
+    self.cursorID = kListDataStoreRefreshCursor;
+    SURLRequest *_request = [[SURLRequest alloc] initWithRequestDelegate:self];
+    _request.tag = SURLRequestItemsRefresh;
+    [_request prepareRequestWith:url CachePolicy:cachePolicy];
+    self.listRequest = _request;
+    [_request release];
+    
+    [self startRequest:self.listRequest];
+}
+- (void)loadMoreItemsWithURL:(NSString *)url {
+    [self cancelRequest:self.listRequest];
+    
+    SURLRequest *_request = [[SURLRequest alloc] initWithRequestDelegate:self];
+    _request.tag = SURLRequestItemsLoadmore;
+    [_request prepareRequestWith:url CachePolicy:TTURLRequestCachePolicyNetwork];
+    self.listRequest = _request;
+    [_request release];
+    
+    [self startRequest:self.listRequest];
+}
 
 @end
