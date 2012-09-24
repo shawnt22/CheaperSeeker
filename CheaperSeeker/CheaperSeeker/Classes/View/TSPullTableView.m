ed1c5b3a6a6393b71d3db6e4539d6c6b27b5420d
tengsong <shawnt22@gmail.com>
2012-09-24 15:47:41 +0800

    调整上拉加载更多样式

--- CheaperSeeker/CheaperSeeker/Classes/View/TSPullTableView.m
+++ CheaperSeeker/CheaperSeeker/Classes/View/TSPullTableView.m
@@ -446,13 +446,13 @@
 - (id)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
-        self.backgroundColor = [UIColor colorWithRed:(37/255.0) green:(37/255.0) blue:(37/255.0) alpha:1.0];
+        self.backgroundColor = [UIColor clearColor];
         
         UILabel *_content = [[UILabel alloc] initWithFrame:CGRectMake(ceilf((frame.size.width-160)/2), frame.size.height-20-16, 160, 20)];
         _content.backgroundColor = [UIColor clearColor];
         _content.textAlignment = UITextAlignmentCenter;
         _content.font = [UIFont systemFontOfSize:14];
-        _content.textColor = [UIColor whiteColor];
+        _content.textColor = [UIColor darkTextColor];
         //_content.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin;
         [self addSubview:_content];
         [_content release];
