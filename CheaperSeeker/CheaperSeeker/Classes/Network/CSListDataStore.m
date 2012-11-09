0d1914d9a84d5f17b3767c372b1f3c8e7307917d
tengsong <shawnt22@gmail.com>
2012-11-09 16:24:23 +0800

    category列表内子category列表默认插入父category

--- CheaperSeeker/CheaperSeeker/Classes/Network/CSListDataStore.m
+++ CheaperSeeker/CheaperSeeker/Classes/Network/CSListDataStore.m
@@ -67,6 +67,10 @@
 @end
 
 #pragma mark - Category
+@interface CSCategoriesDataStore()
+- (NSMutableDictionary *)superCategoryWith:(id)category;
+- (void)appendSuperCategoryWith:(NSMutableArray *)items;
+@end
 @implementation CSCategoriesDataStore
 
 - (void)refreshItemsWithCachePolicy:(ASICachePolicy)cachePolicy {
@@ -74,6 +78,67 @@
 }
 - (void)loadmoreItems {
     [self loadMoreItemsWithURL:[SURLProxy getCategoriesWithCursor:self.cursorID Count:self.limitCount]];
+}
+- (NSMutableDictionary *)superCategoryWith:(id)category {
+    NSMutableDictionary *_mdic = [NSMutableDictionary dictionary];
+    id _v = [category objectForKey:k_category_id];
+    if (_v) {
+        [_mdic setObject:_v forKey:k_category_id];
+    }
+    _v = [category objectForKey:k_category_title];
+    if (_v) {
+        [_mdic setObject:_v forKey:k_category_title];
+    }
+    _mdic = [[_mdic allKeys] count] > 0 ? _mdic : nil;
+    return _mdic;
+}
+- (void)appendSuperCategoryWith:(NSMutableArray *)items {
+    for (NSInteger index = 0; index < [items count]; index++) {
+        NSMutableDictionary *_categoryDic = [NSMutableDictionary dictionaryWithDictionary:[items objectAtIndex:index]];
+        NSMutableArray *_subCategories = [NSMutableArray arrayWithArray:[_categoryDic objectForKey:k_category_subitems]];
+        [_subCategories insertObject:[self superCategoryWith:_categoryDic] atIndex:0];
+        [_categoryDic setObject:_subCategories forKey:k_category_subitems];
+        [items replaceObjectAtIndex:index withObject:_categoryDic];
+    }
+}
+- (void)requestFinished:(ASIHTTPRequest *)asirequest {
+    SURLRequest *request = [self prepareRequest:(SURLRequest *)asirequest];
+    if (!request.error) {
+        switch (request.tag) {
+            case SURLRequestItemsRefresh:{
+                NSArray *_items = [request.formatedResponse objectForKey:kListDataStoreResponseItems];
+                NSInteger _count = [[request.formatedResponse objectForKey:kListDataStoreResponseCount] integerValue];
+                if ([_items count] > 0) {
+                    [self notifyDataloader:self submitResponse:_items Request:request];
+                    self.items = [NSMutableArray arrayWithArray:_items];
+                    if (self.storeStyle == PListDataStoreStyleCursor) {
+                        self.cursorID = [request.formatedResponse objectForKey:kListDataStoreResponseCursor];
+                    }
+                }
+                _canLoadMore = _count > [_items count] ? YES : NO;
+                [self appendSuperCategoryWith:self.items];
+            }
+                break;
+            case SURLRequestItemsLoadmore:{
+                NSArray *_items = [request.formatedResponse objectForKey:kListDataStoreResponseItems];
+                NSInteger _count = [[request.formatedResponse objectForKey:kListDataStoreResponseCount] integerValue];
+                if ([_items count] > 0) {
+                    [self notifyDataloader:self submitResponse:_items Request:request];
+                    [self.items addObjectsFromArray:_items];
+                    if (self.storeStyle == PListDataStoreStyleCursor) {
+                        self.cursorID = [request.formatedResponse objectForKey:kListDataStoreResponseCursor];
+                    }
+                }
+                _canLoadMore = _count > [_items count] ? YES : NO;
+            }
+                break;
+            default:
+                break;
+        }
+        [self notifyDataloader:self didFinishRequest:request];
+    } else {
+        [self notifyDataloader:self didFailRequest:request Error:request.error];
+    }
 }
 
 @end
